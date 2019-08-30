0f4af544ef9636538d73395f7591f886d2ae7502
Alex Newman <alex.newman@armateus.com.br>
2019-08-30 14:21:08 -0300

    UPDT. CoreScrapeException module.

---
+++ corescrape/core/exceptions.py
@@ -0,0 +1,22 @@
+"""
+Core Scrape Exception Module
+
+Implements corescrape exceptios for specific use in the package
+"""
+
+class CoreScrapeException(Exception):
+    """CoreScrape base exception."""
+
+    def __init__(self, msg):
+        """Constructor."""
+
+        self.msg = msg
+        super().__init__(msg)
+
+class CoreScrapeTimeout(CoreScrapeException):
+    """Timeout exception."""
+
+    def __init__(self):
+        """Constructor."""
+
+        super().__init__('Timeout limit reached')
